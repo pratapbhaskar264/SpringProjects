fb452ee4eeb6811e70651fcaabd34d3a97bf157d
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-08-31 14:26:34 +0530

    Initial Commit

---
+++ Oauth2Spring/GoogleGitLogin/src/main/java/com/bhaskar/GoogleGitLogin/GoogleGitLoginApplication.java
@@ -0,0 +1,13 @@
+package com.bhaskar.GoogleGitLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleGitLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleGitLoginApplication.class, args);
+	}
+
+}
---
+++ Oauth2Spring/GoogleGitLogin/src/test/java/com/bhaskar/GoogleGitLogin/GoogleGitLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhaskar.GoogleGitLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleGitLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
