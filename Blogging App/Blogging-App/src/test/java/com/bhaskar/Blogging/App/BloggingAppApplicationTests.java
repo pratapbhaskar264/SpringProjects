e30cd0d6899e76ac492499aa7dd8d7cbffccfec5
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-10-18 11:39:16 +0530

    Started-Blogging-Plateform

---
+++ Blogging App/Blogging-App/src/test/java/com/bhaskar/Blogging/App/BloggingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhaskar.Blogging.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
