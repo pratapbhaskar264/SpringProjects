4ae839accd3fbb5535a6dc3b16cbfcfec1ac2360
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-10-18 13:05:00 +0530

    Tested-ddl

---
+++ Blogging App/Blogging-App/src/main/java/com/bhaskar/Blogging/App/entity/TestClass.java
@@ -0,0 +1,11 @@
+package com.bhaskar.Blogging.App.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class TestClass {
+
+    @Id
+    private Long id;
+}
