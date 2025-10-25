405aa449f6520af30d882f2aaf04f1817a1c3b7c
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-10-25 21:02:32 +0530

    db-Tables-via-JPA

---
+++ Hospital-Management/src/main/java/com/bhaskar/Hospital_Management/models/Appointment.java
@@ -0,0 +1,25 @@
+package com.bhaskar.Hospital_Management.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor //lombok
+@NoArgsConstructor
+@Getter
+@Setter
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long patientId;
+    private Long doctorId ;
+    private String date;
+
+
+}
