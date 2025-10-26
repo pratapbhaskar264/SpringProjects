1bb6b4dd0b22add8c0a2810efc85ef4882b4b738
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-10-26 20:35:41 +0530

    Changes-saved

--- Hospital-Management/src/main/java/com/bhaskar/Hospital_Management/controllers/Patientcontrollers.java
+++ Hospital-Management/src/main/java/com/bhaskar/Hospital_Management/controllers/Patientcontrollers.java
@@ -1,5 +1,27 @@
 package com.bhaskar.Hospital_Management.controllers;
 
+import com.bhaskar.Hospital_Management.models.Patient;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.service.annotation.PatchExchange;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/patients")
 public class Patientcontrollers {
+
+
+
+    @GetMapping
+    public List<Patient> getAllPatients(){
+        return new ArrayList<>();
+    }
+
+    @PostMapping
+    public Patient createPatient(@RequestBody Patient patient) {
+        System.out.println("woohhho...mreejBngya");
+        return new Patient();
+    }
 }
