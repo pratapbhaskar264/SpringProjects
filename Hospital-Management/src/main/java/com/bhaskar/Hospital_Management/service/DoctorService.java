3359768fa5650c0478c7c68feed1c6b466282d35
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-10-28 22:09:46 +0530

    changed

--- Hospital-Management/src/main/java/com/bhaskar/Hospital_Management/service/DoctorService.java
+++ Hospital-Management/src/main/java/com/bhaskar/Hospital_Management/service/DoctorService.java
@@ -1,22 +1,28 @@
 package com.bhaskar.Hospital_Management.service;
 
+import com.bhaskar.Hospital_Management.Repository.DoctorRepository;
 import com.bhaskar.Hospital_Management.models.Doctor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DoctorService {
 
     private static final Logger logger = LoggerFactory.getLogger(DoctorService.class);
 
+    @Autowired
+    private DoctorRepository doctorRepository;
+
     public List<Doctor> getAllDoctors() {
         try {
             logger.info("SERVICE - getAllDoctors");
-            return new ArrayList<>();
+            return doctorRepository.findAll();
         } catch (Exception e) {
             logger.error("An error occurred while fetching all doctors", e);
             return new ArrayList<>();
@@ -26,17 +32,18 @@ public class DoctorService {
     public Doctor getDoctorById(Long id) {
         try {
             logger.info("SERVICE - getDoctorById: {}", id);
-            return new Doctor();
+            Optional<Doctor> doctor = doctorRepository.findById(id);
+            return doctor.orElse(null);
         } catch (Exception e) {
             logger.error("An error occurred while fetching doctor with ID: {}", id, e);
-            return new Doctor();
+            return null;
         }
     }
 
     public Doctor createDoctor(Doctor doctor) {
         try {
             logger.info("SERVICE - createDoctor");
-            return new Doctor();
+            return doctorRepository.save(doctor);
         } catch (Exception e) {
             logger.error("An error occurred while creating a doctor: {}", doctor, e);
             return new Doctor();
@@ -46,14 +53,26 @@ public class DoctorService {
     public void deleteDoctorById(Long id) {
         try {
             logger.info("SERVICE - deleteDoctorById: {}", id);
+            doctorRepository.deleteById(id);
         } catch (Exception e) {
             logger.error("An error occurred while deleting doctor with ID: {}", id, e);
         }
     }
 
-    public void updateDoctor(Doctor doctor) {
+    public void updateDoctor(Doctor doctor , Long id) {
         try {
             logger.info("SERVICE - updateDoctor");
+            Optional<Doctor> tempdoctor = doctorRepository.findById(id);
+            if(tempdoctor.isPresent()){
+                Doctor p = tempdoctor.get();
+                p.setName(doctor.getName());
+                p.setSpeciality(doctor.getSpeciality());
+
+                doctorRepository.save(p);
+            }
+            else{
+                logger.error("Patient with id : {} not found " , id);
+            }
         } catch (Exception e) {
             logger.error("An error occurred while updating doctor: {}", doctor, e);
         }
